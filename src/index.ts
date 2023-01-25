2b6f68bae6038f51887dc2b838ee2252f4864801
GleitonK <kgleiton@hotmail.com>
2023-01-25 18:29:30 -0300

    Remover 9 do telefone

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,33 @@
 import express, { Request, Response } from 'express'
 import Sender from './sender'
+import { validateAndFormatPhoneNumber } from './util'
 
-const sender = new Sender()
+// const sender = new Sender()
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 
-// app.listen(process.env.PORT); Obrigatorio em produção
-app.listen('333', () => console.log("Live"))
+// app.listen(process.env.PORT); // Obrigatorio em produção
+app.listen('3333', () => console.log("Live"))
 
 
-app.get('/status', (req: Request, res: Response) => {
+// app.get('/status', (req: Request, res: Response) => {
 
-    const connected = sender.isConnected
+//     const connected = sender.isConnected
 
-    if (!connected) {
-        return res.send({
-            qr_code: sender.qrCode,
-            connected: sender.isConnected
-        })
-    }
+//     if (!connected) {
+//         return res.send({
+//             qr_code: sender.qrCode,
+//             connected: sender.isConnected
+//         })
+//     }
 
-    return res.send({
-        connected: sender.isConnected
-    })
-})
+//     return res.send({
+//         connected: sender.isConnected
+//     })
+// })
 
 
 app.post('/send', async (req: Request, res: Response) => {
@@ -34,7 +35,9 @@ app.post('/send', async (req: Request, res: Response) => {
 
         const { number, message } = req.body
 
-        await sender.sendMessage(number, message)
+        validateAndFormatPhoneNumber(number)
+
+        // await sender.sendMessage(number, message)
 
         return res.status(200).json()
     } catch (error) {
@@ -44,16 +47,16 @@ app.post('/send', async (req: Request, res: Response) => {
 })
 
 
-app.post('/sendMultiple', async (req: Request, res: Response) => {
-    try {
-        const { numbers, message } = req.body
+// app.post('/sendMultiple', async (req: Request, res: Response) => {
+//     try {
+//         const { numbers, message } = req.body
 
-        const { listOfInvalidNumbers, not_sent_amount } = await sender.sendMultipleMessages(numbers, message)
+//         const { listOfInvalidNumbers, not_sent_amount } = await sender.sendMultipleMessages(numbers, message)
 
 
-        return res.status(200).json({ listOfInvalidNumbers, not_sent_amount })
-    } catch (error) {
-        console.error(error)
-        res.status(500).json({ status: "error", message: error })
-    }
-})
+//         return res.status(200).json({ listOfInvalidNumbers, not_sent_amount })
+//     } catch (error) {
+//         console.error(error)
+//         res.status(500).json({ status: "error", message: error })
+//     }
+// })
--- src/util/validatePhoneNumber.ts
+++ src/util/validatePhoneNumber.ts
@@ -9,13 +9,18 @@ function validateAndFormatPhoneNumber(stringToCheck: string) {
 
     let phoneNumber = parsePhoneNumber(stringToCheck, "BR")?.format("E.164").replace('+', '') as string
 
+
     if (phoneNumber.length < 12) {
         return false
     }
 
-    const regex = new RegExp(/^(55)\s?([1-9][0-9])((?:[2-9])\d{3})\-?(\d{4})$/)
+    if (phoneNumber.length === 13 && phoneNumber.substring(4, 5) === "9") {
+        phoneNumber = phoneNumber.substring(0, 4) + phoneNumber.substring(5);
+    }
 
-    if (!regex.test(phoneNumber)) {
+    const regexFormatCheck = new RegExp(/^(55)([1-9][0-9])((?:[1-9])\d{3})\-?(\d{4})$/)
+
+    if (!regexFormatCheck.test(phoneNumber)) {
         return false
     }
 

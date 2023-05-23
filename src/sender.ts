c16d7cc81cad059089634b52fe276c1659bf8894
GleitonK <kgleiton@hotmail.com>
2023-05-23 10:35:46 -0300

    Atualização Venom-bot

--- src/sender.ts
+++ src/sender.ts
@@ -1,99 +1,97 @@
-import { create, SocketState, Whatsapp } from 'venom-bot';
-import { validateAndFormatPhoneNumber } from './util';
+import { CatchQR, create, SocketState, Whatsapp } from "venom-bot";
+import { validateAndFormatPhoneNumber } from "./util";
 
 export type QrCode = {
-    base64Qr: string
-    asciiQR: string
-    attempts: number
-}
+  base64Qr: string;
+  asciiQR: string;
+  attempts: number;
+};
 
 export type InvalidNUmbersList = {
-    listOfInvalidNumbers: string[]
-    not_sent_amount: number
-}
+  listOfInvalidNumbers: string[];
+  not_sent_amount: number;
+};
 
 class Sender {
-    private client: Whatsapp
-    private connected: boolean
-    private qr: QrCode
+  private client: Whatsapp;
+  private connected: boolean;
+  private qr: CatchQR;
 
-    get isConnected(): boolean {
-        return this.connected
+  get isConnected(): boolean {
+    return this.connected;
+  }
+
+  get qrCode(): CatchQR {
+    return this.qr;
+  }
+
+  constructor() {
+    this.initialize();
+  }
+
+  private initialize() {
+    const qr = (
+      qrCode: string,
+      asciiQR: string,
+      attempt?: number,
+      urlCode?: string
+    ) => {
+      this.qr = qr;
+    };
+
+    const status = (statusSession: string) => {
+      this.connected = ["IsLogged", "qrReadSuccess", "chatsAvailable"].includes(
+        statusSession
+      );
+    };
+
+    const start = (client: Whatsapp) => {
+      this.client = client;
+
+      client.onStateChange((state) => {
+        this.connected = state === SocketState.CONNECTED;
+      });
+    };
+
+    create("gestao", qr)
+      .then((client) => start(client))
+      .catch((error) => console.error(error));
+  }
+
+  async sendMessage(to: string, body: string) {
+    const validatedNumber = validateAndFormatPhoneNumber(to);
+
+    if (!validatedNumber) {
+      throw "Número Whatsapp inválido!";
     }
 
-    get qrCode(): QrCode {
-        return this.qr
+    await this.client.sendText(validatedNumber, body);
+  }
+
+  async sendMultipleMessages(
+    listOfPhoneNumbers: string[],
+    body: string
+  ): Promise<InvalidNUmbersList> {
+    let listOfInvalidNumbers: string[] = [];
+
+    for (const phone of listOfPhoneNumbers) {
+      const validatedNumber = validateAndFormatPhoneNumber(phone);
+
+      if (!validatedNumber) {
+        listOfInvalidNumbers.push(phone);
+        continue;
+      }
+
+      await this.client.sendText(validatedNumber, body).catch(() => {
+        listOfInvalidNumbers.push(phone);
+      });
     }
 
-    constructor() {
-        this.initialize()
-    }
-
-    private initialize() {
-
-        const qr = (
-            base64Qr: string,
-            asciiQR: string,
-            attempts: number
-        ) => {
-            this.qr = { base64Qr, attempts, asciiQR }
-        }
-
-        const status = (statusSession: string) => {
-            this.connected = ["IsLogged", "qrReadSuccess", "chatsAvailable"]
-                .includes(statusSession)
-        }
-
-        const start = (client: Whatsapp) => {
-            this.client = client
-
-            client.onStateChange(state => {
-                this.connected = state === SocketState.CONNECTED
-            })
-        }
-
-        create('gestao', qr)
-            .then((client) => start(client))
-            .catch((error) => console.error(error));
-    }
-
-    async sendMessage(to: string, body: string) {
-
-        const validatedNumber = validateAndFormatPhoneNumber(to)
-
-        if (!validatedNumber) {
-            throw 'Número Whatsapp inválido!'
-        }
-
-        await this.client.sendText(validatedNumber, body)
-    }
-
-
-    async sendMultipleMessages(listOfPhoneNumbers: string[], body: string): Promise<InvalidNUmbersList> {
-        let listOfInvalidNumbers: string[] = [];
-
-        for (const phone of listOfPhoneNumbers) {
-
-            const validatedNumber = validateAndFormatPhoneNumber(phone)
-
-
-            if (!validatedNumber) {
-                listOfInvalidNumbers.push(phone)
-                continue
-            }
-
-            await this.client.sendText(validatedNumber, body)
-                .catch(() => {
-                    listOfInvalidNumbers.push(phone)
-                })
-        }
-
-        return { listOfInvalidNumbers, not_sent_amount: listOfInvalidNumbers.length }
-    }
-
-
-
+    return {
+      listOfInvalidNumbers,
+      not_sent_amount: listOfInvalidNumbers.length,
+    };
+  }
 }
 
-
-export default Sender 
+export default Sender;
